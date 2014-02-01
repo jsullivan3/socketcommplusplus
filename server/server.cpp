40366b973f695db6dab0654f7d99bb3635e9b5a8
John Sullivan <jsullivan3@gmail.com>
2014-01-31 20:31:59 -0500

    Basic "Hello, world!" framework for server.

---
+++ server/server.cpp
@@ -0,0 +1,36 @@
+// @file  server.c
+// @brief Everything you need for a sample server.
+//
+// Copyright (C) 2014 John F. Sullivan, III
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+#include <iostream>
+
+using namespace std;
+
+// main() - create a server thread.
+//
+// @param argc Number of arguments to the executable.
+// @param argv Array of arguments to the executable.
+//
+// @return @c 0 on success, @c errno value on error
+int main(int argc, char **argv)
+{
+    int status = 0;
+    cout << "=== Exiting with status " << status << endl;
+
+    return(status);
+}
